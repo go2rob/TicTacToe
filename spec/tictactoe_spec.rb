80267b6c03cbd4b294a4019a27f599d4c31e20cf
go2rob <robmail1996@gmail.com>
2017-07-13 17:18:08 +0530

    First commit of TicTacToe

---
+++ spec/tictactoe_spec.rb
@@ -0,0 +1,15 @@
+describe "TicTacToe" do
+
+	it "should make the board"do
+
+	end
+	context "The game" do
+		it "should have 2 distinct symbols"
+		it "should "
+	end
+	context "win" do
+		it "declare win if aligned horiontally"
+		it "declare win if aligned vertically"
+		it "declare win if aligned diagonally"
+	end
+end
